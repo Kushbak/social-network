9ea1f08f60608ba1416140d6b928548f39bf2a91
Kushbak Mamytov <mamytovkushbak@gmail.com>
2020-02-26 21:34:58 +0600

    Division into components, added css-styles

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+let Header = () => {
+    return (
+        <header>
+            <img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F4.bp.blogspot.com%2F-xRqe8bLNdkI%2FUWko99o1qGI%2FAAAAAAAADPg%2Fse7eP_ss_Eo%2Fs640%2FBoston_Bruins_logo.gif&f=1&nofb=1" alt="logo" />
+        </header>
+    )
+}
+
+export default Header;
