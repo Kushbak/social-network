6d4e3017b8c7b7e3a416c82d643cd4c32027238c
Kushbak <mamytovkushbak@gmail.com>
2020-02-29 20:45:19 +0600

    added props for components

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,21 +2,23 @@ import React from 'react';
 import p from './MyPosts.module.css';
 import Post from './Post/Post';
 
-let MyPosts = () => {
-    return ( 
+// задаем значения для их динамического вывода с помощью props 
+let MyPosts = (props) => {
+    return (
+        <div>
+            My posts
+                <div>
+                <textarea cols="20" rows="1"></textarea>
+                <button>add post</button>
+            </div>
             <div>
-                My posts 
-                <div>
-                    <textarea cols="20" rows="1"></textarea>
-                    <button>add post</button>
-                </div>
-                <div>
-                    <Post />
-                    <Post />
-                    <Post />
-                    <Post />
-                </div>
-            </div> 
+
+                <Post message="Hi, how are you?" likes="34 likes" />
+                <Post message="It is my first post" likes="24 likes" />
+                <Post message="It is my second post and I wanna experiment with it" likes="10 likes" />
+                <Post message="Like my post please" likes="0 likes" />
+            </div>
+        </div>
     )
 }
 
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import p from './Post.module.css';
-
-let Post = () => {
-    return (  
-                    <div className={p.item}>
-                        <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2F2%2F2f%2FAlesso_profile.png&f=1&nofb=1" alt="ava"/>
-                        Post 1
-                        <div>
-                            <span>like</span>    
-                        </div>    
-                    </div> 
+// props - атрибут для компоненты, при вызывании в jsx задается как аргумент(html), а сохраняется как объект, примеры ниже и в файле MyPosts.jsx
+let Post = (props) => {
+    return (
+        <div className={p.item}>
+            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2F2%2F2f%2FAlesso_profile.png&f=1&nofb=1" alt="ava" />
+            {props.message}
+            <div>
+                <span>{props.likes}</span>
+            </div>
+        </div>
     )
 }
 
