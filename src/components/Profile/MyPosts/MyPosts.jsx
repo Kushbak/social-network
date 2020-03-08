14ba7eab47fad5be67fc646165535620edbcf81d
Kushbak <mamytovkushbak@gmail.com>
2020-03-09 05:02:30 +0600

    added project to github repository, if I did it correctly

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import css from './MyPosts.module.css';
 import Post from './Post/Post';
-
-//  BLL/UI
-
-
-
-
+ 
+// createRef - создание ссылки к которому нужно обратиться за value, не используя DOM
+// задаем ref как ссылку туда и обращаемся с функцией
+// при клике на addPost вызывается функция, которая берет значения из textarea, ссылка которой действует с ref
  
 const MyPosts = (props) => {
     let postsElements = props.postsData.map( p => <Post id={p.id} message={p.message} likes={p.likes} /> );
 
+    let newPostElement = React.createRef();
+    let addPost = () => {
+        let text = newPostElement.current.value; 
+
+        alert(text);
+    }
 
     return (
         
@@ -18,10 +22,10 @@ const MyPosts = (props) => {
             <h3>My posts</h3>
             <div className={css.addPost}>
                 <div> 
-                    <textarea cols="20" rows="1.5"></textarea>
+                    <textarea ref={newPostElement} cols="20" rows="1.5"></textarea>
                 </div>
                 <div>
-                    <button>add post</button> 
+                    <button onClick={addPost}>add post</button> 
                 </div>
             </div>
             <div className={css.postsBlock}> 
