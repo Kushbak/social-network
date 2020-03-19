e4f4b548ca26fe0e14f24df9633345f26b628494
Kushbak <mamytovkushbak@gmail.com>
2020-03-19 13:57:55 +0600

    added redux-store and some refactoring

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import * as serviceWorker from './serviceWorker';
 import React from 'react';
 import ReactDOM from 'react-dom'; 
 import App from './App'; 
-import store from './redux/state'; 
+import store from './redux/redux-store'; 
 import {  BrowserRouter } from 'react-router-dom';
 
 
@@ -10,15 +10,18 @@ let rerenderDOM = (state) => {
         ReactDOM.render(
             <BrowserRouter>
                 <App 
-                        state={state} 
+                        store={store} 
                         dispatch={store.dispatch.bind(store)}  /> 
             </BrowserRouter>  , document.getElementById('root'));
             
 };   
 
 rerenderDOM(store.getState());
-
-store.subscribe(rerenderDOM);
+ 
+store.subscribe( () => {
+    let state = store.getState();
+    rerenderDOM(state);
+});
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
