a3aa833513b816112778891e66c37c030d2b954e
Kushbak <mamytovkushbak@gmail.com>
2020-03-19 13:58:33 +0600

    added containers for some components

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
-import Dialogs from './components/Dialogs/Dialogs';
+import DialogsContainer from './components/Dialogs/DialogsContainer';
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
 import {  Route } from 'react-router-dom';
+import store from './redux/redux-store';
 
 // route для маршрутизации, или как ссылка меняет контент в блоке 'контент'
 // для работы должна быть обернута в тег <BrowserRouter>, указывается путь, и какая компонента должна рисоваться
@@ -17,10 +18,8 @@ let App = (props) => {
                <Header />
                <Navbar />
                <div className="app-wrapper-content">
-                    <Route path='/profile'   render={ () => <Profile profilePage={props.state.profilePage}  
-                                                                                          dispatch={props.dispatch}/> } />
-                    <Route path='/dialogs'  render={ () => <Dialogs state={props.state.dialogsPage}  
-                                                                                          dispatch={props.dispatch} /> } />  
+                    <Route path='/profile'   render={ () => <Profile store={props.store} /> } />
+                    <Route path='/dialogs'  render={ () => <DialogsContainer store={props.store} />  } />  
                     <Route path='/news'     render={ () => <News /> } />  
                     <Route path='/music'    render={ () => <Music /> } />  
                     <Route path='/settings' render={ () => <Settings /> } />  
