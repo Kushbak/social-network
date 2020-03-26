1189d47501f4094bc409d06a04b5b4f9cb85bba7
Kushbak <mamytovkushbak@gmail.com>
2020-03-26 14:13:56 +0600

    usersContainer's import

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import DialogsContainer from './components/Dialogs/DialogsContainer';
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
-import {  Route } from 'react-router-dom';
-import store from './redux/redux-store';
+import {  Route } from 'react-router-dom';  
+import UsersContainer from './components/Users/UsersContainer';
 
 // route для маршрутизации, или как ссылка меняет контент в блоке 'контент'
 // для работы должна быть обернута в тег <BrowserRouter>, указывается путь, и какая компонента должна рисоваться
@@ -18,8 +18,9 @@ let App = (props) => {
                <Header />
                <Navbar />
                <div className="app-wrapper-content">
-                    <Route path='/profile'   render={ () => <Profile store={props.store} /> } />
-                    <Route path='/dialogs'  render={ () => <DialogsContainer store={props.store} />  } />  
+                    <Route path='/profile'  render={ () => <Profile /> } />
+                    <Route path='/dialogs'  render={ () => <DialogsContainer />  } />  
+                    <Route path='/users'    render={ () => <UsersContainer />  } />  
                     <Route path='/news'     render={ () => <News /> } />  
                     <Route path='/music'    render={ () => <Music /> } />  
                     <Route path='/settings' render={ () => <Settings /> } />  
