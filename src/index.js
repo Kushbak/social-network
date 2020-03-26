4065d8181f374ac0e96e8f4081acc68d31db1289
Kushbak <mamytovkushbak@gmail.com>
2020-03-26 14:15:35 +0600

    removed all rendering code

--- src/index.js
+++ src/index.js
@@ -3,25 +3,17 @@ import React from 'react';
 import ReactDOM from 'react-dom'; 
 import App from './App'; 
 import store from './redux/redux-store'; 
-import {  BrowserRouter } from 'react-router-dom';
+import {  BrowserRouter } from 'react-router-dom'; 
+import { Provider } from 'react-redux';
 
-
-let rerenderDOM = (state) => {
-        ReactDOM.render(
-            <BrowserRouter>
-                <App 
-                        store={store} 
-                        dispatch={store.dispatch.bind(store)}  /> 
-            </BrowserRouter>  , document.getElementById('root'));
-            
-};   
-
-rerenderDOM(store.getState());
  
-store.subscribe( () => {
-    let state = store.getState();
-    rerenderDOM(state);
-});
+ReactDOM.render(
+    <BrowserRouter>
+        <Provider store={store}>
+            <App/> 
+        </Provider>
+    </BrowserRouter>  , document.getElementById('root'));
+        
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
