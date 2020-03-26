c90092907d08960d0f1b53c34ed8b91e7798e1d6
Kushbak <mamytovkushbak@gmail.com>
2020-03-26 14:16:58 +0600

    added NavLink for usersPage

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -17,6 +17,9 @@ let Navbar = () => {
                 <NavLink to="/dialogs" activeClassName={css.activeLink}>Messages</NavLink>
             </div>
             <div className={css.item}>
+                <NavLink to="/users" activeClassName={css.activeLink}>Users</NavLink>
+            </div>
+            <div className={css.item}>
                 <NavLink to="/news" activeClassName={css.activeLink}>News</NavLink>
             </div>
             <div className={css.item}>
