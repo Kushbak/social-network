ac7b56254941feb26df2be58eb285bbb9b20aa82
Mamytov Kushbak <mamytovkushbak@gmail.com>
2020-03-29 05:25:20 +0600

    added preloader for stateFetching

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,42 +1,48 @@
 import React from 'react';
 import css from './Users.module.css';
-import * as axios from 'axios';
 import userAvatar from '../../assets/images/user.png'
+import Prelodader from '../common/Preloader/Preloader';
 
 
-class Users extends React.Component {
-    constructor(props) {
-        super(props);
-        axios.get("https://social-network.samuraijs.com/api/1.0/users")
-            .then(response => {
-                this.props.setUsers(response.data.items);
-            });
+
+const Users = (props) => {
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pagesSize)
+
+    let pages = [];
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i);
     }
-
-    render() {
-        return (
-            <div>
+    return (<>
+        {props.isFetching ? <Prelodader /> : null}
+        <div>
+            <div className={css.forChangingPages}>
                 {
-                    this.props.users.map(u => <div key={u.id} className={css.block}>
-                        <div>
-                            <img className={css.avatar} src={u.photos.small != null ? u.photos.small : userAvatar} alt="avatar" />
-                            {u.isFollow
-                                ? <button className={css.isFollowBtn} onClick={() => { this.props.unFollow(u.id) }}>Unfollow</button>
-                                : <button className={css.isUnfollowBtn} onClick={() => { this.props.follow(u.id) }}>Follow</button>}
-                        </div>
-                        <div className={css.descrBlock}>
-                            <div className={css.fullName}>{u.name}</div>
-                            <div className={css.status}>{u.status}</div>
-                            <div className={css.location}>
-                                {"u.location.city"},
-                            {"u.location.country"}
-                            </div>
-                        </div>
-                    </div>)
+                    pages.map(p => {
+                        return <button onClick={(e) => { props.onChangeCurrentPage(p) }} className={props.currentPage === p && css.selectedPage}> {p} </button>
+                    })
                 }
             </div>
-        )
-    };
+            {
+                props.users.map(u => <div key={u.id} className={css.block}>
+                    <div>
+                        <img className={css.avatar} src={u.photos.small != null ? u.photos.small : userAvatar} alt="avatar" />
+                        {u.isFollow
+                            ? <button className={css.isFollowBtn} onClick={() => { props.unFollow(u.id) }}>Unfollow</button>
+                            : <button className={css.isUnfollowBtn} onClick={() => { props.follow(u.id) }}>Follow</button>}
+                    </div>
+                    <div className={css.descrBlock}>
+                        <div className={css.fullName}>{u.name}</div>
+                        <div className={css.status}>{u.status}</div>
+                        <div className={css.location}>
+                            {"u.location.city"},
+                        {"u.location.country"}
+                        </div>
+                    </div>
+                </div>)
+            }
+        </div>
+    </>
+    )
 }
 
 export default Users;
