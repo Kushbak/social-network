0f2b930665c74af71947b290566d380270a8f38b
Kushbak Mamytov <mamytovkushbak@gmail.com>
2020-04-09 23:55:42 +0600

    DAL

---
+++ src/api/api.js
@@ -0,0 +1,18 @@
+import * as axios from 'axios';
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        "API-KEY": "729e5af1-8801-4361-bfc9-77d08357072e"
+    }
+})
+
+export const usersApi = {
+    getUsers(currentPage = 1, pageSize = 10) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+            .then(response => {
+                return response.data
+            });
+    }
+}
