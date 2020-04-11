447d19d9d02c6a41b5a22fea985380a56e8831eb
Kushbak Mamytov <mamytovkushbak@gmail.com>
2020-04-11 23:10:21 +0600

    added status and name of user

--- src/components/Profile/Description/Description.jsx
+++ src/components/Profile/Description/Description.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
-import css from './Description.module.css';
+import styles from './Description.module.css';
 import Preloader from '../../common/Preloader/Preloader';
 import defaultAva from '../../../assets/images/avatar.jpg'
+import ProfileStatus from './ProfileStatus/ProfileStatus';
 const Description = (props) => {
     if (!props.profile) {
         return <Preloader />
     }
-
     return (
-        <div className={css.descrWrapper}>
-            <div className={css.descrBlock}>
-                <div className={css.avatar}>
-                    <img src={
-                        props.profile.photos.large !== null ? props.profile.photos.large : defaultAva} alt="avatar" /> 
-                </div>
-                <div><p>{props.profile.fullName}</p></div>
+        <div className={styles.descrWrapper}>
+            <div className={styles.avatar}>
+                <img src={
+                    props.profile.photos.large !== null ? props.profile.photos.large : defaultAva} alt="avatar" /> 
+            </div>
+            <div className={styles.aboutMe}>
+                <p className={styles.userName}>{props.profile.fullName}</p>
+                <ProfileStatus status={props.profile.lookingForAJobDescription}/>
             </div>
         </div>
     );
