10956e2122b4a85c6abd8cfe769c261820430e0e
Kushbak Mamytov <mamytovkushbak@gmail.com>
2020-04-20 20:19:35 +0600

    added action for setting datas to profile

--- src/redux/authReducer.js
+++ src/redux/authReducer.js
@@ -9,47 +9,53 @@ let initialState = {
 
 export const authReducer = (state = initialState, action) => {
 
-    switch(action.type){ 
-        case 'SET_AUTH_USER_DATA':{
+    switch (action.type) {
+        case 'SET_AUTH_USER_DATA': {
             return {
                 ...state,
-                ...action.payload 
-            } 
+                ...action.payload
+            }
+        }        
+        case 'SET_USER_PROFILE': {
+            return {
+                ...state,
+                profile: action.profile
+            }
         }
         default:
             return state;
-    } 
+    }
 
 };
+export const setAuthUserData = (userId, email, login, isAuth) => ({
+    type: 'SET_AUTH_USER_DATA',
+    payload: { userId, email, login, isAuth }
+});
 
-
-export const setAuthUserData = (userId, email, login, isAuth) => ({ type: 'SET_AUTH_USER_DATA', payload: {userId, email, login, isAuth} });
 
 
 export const authUserDataThunk = () => (dispatch) => {
     authApi.me()
-    .then(response => { 
-        if(response.data.resultCode === 0){
-            let {id, email, login} = response.data.data
-            dispatch(setAuthUserData(id, email, login, true));
-        }
-    });
+        .then(response => {
+            if (response.data.resultCode === 0) {
+                let { id, email, login } = response.data.data
+                dispatch(setAuthUserData(id, email, login, true));
+            }
+        });
 }
-
 export const login = (email, password, rememberMe) => (dispatch) => {
     authApi.login(email, password, rememberMe, true)
-    .then(response => {  
-        if(response.data.resultCode === 0){
-            dispatch(authUserDataThunk())
-        }
-    });
+        .then(response => {
+            if (response.data.resultCode === 0) {
+                dispatch(authUserDataThunk())
+            }
+        });
 }
-
 export const logout = () => (dispatch) => {
     authApi.logout()
-    .then(response => { 
-        if(response.data.resultCode === 0){
-            dispatch(setAuthUserData(null, null, null, false))
-        }
-    });
+        .then(response => {
+            if (response.data.resultCode === 0) {
+                dispatch(setAuthUserData(null, null, null, false))
+            }
+        });
 }
