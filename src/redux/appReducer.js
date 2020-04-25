b114100b31d92ce9178d97f3a0c4daae3dfbeb72
Kushbak Mamytov <mamytovkushbak@gmail.com>
2020-04-26 03:22:04 +0600

    created reducer for initializing user

---
+++ src/redux/appReducer.js
@@ -0,0 +1,31 @@
+import { authUserDataThunk } from "./authReducer";
+
+let initialState = {
+    initialized: false
+};
+
+export const appReducer = (state = initialState, action) => {
+
+    switch (action.type) {
+        case 'INITIALIZED_SUCCESS': {
+            return {
+                ...state,
+                initialized: true
+            }
+        }
+        default:
+            return state;
+    }
+
+};
+
+export const initializedSuccess = () => ({ type: 'INITIALIZED_SUCCESS' });
+
+export const initializeApp = () => (dispatch) => {
+    let promise = dispatch(authUserDataThunk())
+    debugger
+    promise.then(() => {
+        dispatch(initializedSuccess())
+    });
+
+} 
