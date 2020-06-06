fb819c49983e54a7747d17adefde4f3d0725c6c2
Kushbak Mamytov <mamytovkushbak@gmail.com>
2020-06-06 15:24:44 +0600

    for creating advanced form fields

---
+++ src/components/common/FormControls/FormControls.js
@@ -0,0 +1,8 @@
+export const createField = (placeholder, name, validators, component, props = [], text = '') => <div>
+        <Field placeholder={placeholder}
+                name={name}
+                validate={validators}
+                component={component}
+                {...props}    
+        />{text}
+    </div> 
