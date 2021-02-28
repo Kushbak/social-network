7e2bb3b6ff16e5afaecd13107d9b13cfba3454d3
Kushbak Mamytov <mamytovkushbak@gmail.com>
2021-02-28 15:12:50 +0600

    some changes

--- src/components/common/Paginator/Paginator.js
+++ src/components/common/Paginator/Paginator.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import styles from './Paginator.module.css';
 
 const Paginator = (props) => {
-    let portionSize = 10
+    let portionSize = 30
     let pagesCount = Math.ceil(props.totalUsersCount / props.pagesSize)
 
     let pages = [];
@@ -10,26 +10,26 @@ const Paginator = (props) => {
         pages.push(i);
     }
 
-    // let portionCount = Math.ceil(pagesCount / portionSize)
-    // let [portionNumber, setPortionNumber] = useState(1)
-    // let leftPortionNumber = (portionNumber - 1) * portionSize + 1
-    // let rightPortionNumber = portionNumber * portionSize
+    let portionCount = Math.ceil(pagesCount / portionSize)
+    let [portionNumber, setPortionNumber] = useState(1)
+    let leftPortionNumber = (portionNumber - 1) * portionSize + 1
+    let rightPortionNumber = portionNumber * portionSize
 
     return (
         <div className={styles.Paginator}>
-            {/* {portionNumber > 1 &&
-                <button onClick={() => { setPortionNumber(portionNumber - 1) }}>LEFT</button>} */}
+            {portionNumber > 1 &&
+                <button onClick={() => { setPortionNumber(portionNumber - 1) }}>LEFT</button>}
             <div className={styles.forChangingPages}>
                 {
                     pages
-                        // .filter(p => p >= leftPortionNumber && p <= rightPortionNumber)
+                        .filter(p => p >= leftPortionNumber && p <= rightPortionNumber)
                         .map(p => {
                             return <button onClick={(e) => { props.onChangeCurrentPage(p) }} className={props.currentPage === p && styles.selectedPage}> {p} </button>
                         })
                 }
             </div>
-            {/* {portionCount > portionNumber && 
-                <button onClick={() => {setPortionNumber(portionNumber + 1)}}>RIGHT</button>} */}
+            {portionCount > portionNumber && 
+                <button onClick={() => {setPortionNumber(portionNumber + 1)}}>RIGHT</button>}
         </div>
     )
 }
